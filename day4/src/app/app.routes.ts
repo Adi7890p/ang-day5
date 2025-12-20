44e4db1976760e26a2801a12bce4daec967237ec
Adi7890p <113298694+Adi7890p@users.noreply.github.com>
2025-12-20 14:46:20 +0530

    day 4

---
+++ day4/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {path:"home",
+        loadComponent:()=>import('./home/home.component').then(c=>c.HomeComponent)
+    },
+    {path:"profile",
+        loadComponent:()=>import('./profile/profile.component').then(c=>c.ProfileComponent)
+    }
+];
